42adfa1c2a1460a6bb6d5e27fc70fc6e0d9d18e1
Omar2535 <omar2535@alumni.ubc.ca>
2023-04-01 00:16:41 -0700

    Initial website home page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,29 @@
 // components/Navbar.tsx
 import React from "react";
+import Image from 'next/image'
 import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100">
-      <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">My Website</a>
+    <nav className="bg-gray-900 text-white py-3 md:py-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href="#" className="text-lg font-bold">
+          <Image src="/logo.png" alt="TTSec" width={50} height={50}></Image>
+        </Link>
+        <button className="md:hidden">
+          <svg className="w-6 h-6 fill-current" viewBox="0 0 24 24">
+            <path
+              fill-rule="evenodd"
+              d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"
+            ></path>
+          </svg>
+        </button>
+        <div className="hidden md:flex md:items-center">
+          <Link href="/" className="block md:inline-block mt-4 md:mt-0 mr-6">Home</Link>
+          <Link href="/about" className="block md:inline-block mt-4 md:mt-0 mr-6">About</Link>
+        </div>
       </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/contact">Contact</Link>
-          </li>
-          <li>
-            <Link href="/about">About</Link>
-          </li>
-        </ul>
-      </div>
-    </div>
+    </nav>
   );
 };
 export default Navbar
